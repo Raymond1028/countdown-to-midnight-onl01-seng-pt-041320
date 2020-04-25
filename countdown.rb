64c408fc3c0e5498d319320c3b90ccf8c5ceed5d
Raymond Sims <lancelot53.rs@gmail.com>
2020-04-25 18:58:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  int = 10
+  while int > 0
+    puts"#{int} SECOND(S)!\n"
+    #sleep 1
+    int -=1
+  end
+return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  int = 10
+  while int > 0
+    puts"#{int} SECOND(S)!\n"
+    sleep 1
+    int -=1
+  end
+return "HAPPY NEW YEAR!"
+end
+
+
